1a849148dbdf9702f7136c33f014930d4e3a0307
soakit <liuchang2024@gmail.com>
2018-07-24 11:53:27 +0800

    optimize code

--- README.md
+++ README.md
@@ -2,8 +2,22 @@
 
 0. `npm install`
 
-1. edit config in `config.js` and `index.js` if need
+1. `config.js` to write your book setting
 
-2. `npm run start`
-
+2. `node index.js`
+```
+    -o, --host [value]          host
+    -n, --novelId [value]       书籍章节链接针对ID动态的
+    -s, --staticId [value]      书籍章节链接针对ID静态的
+    -c, --maxConnections <n>    最大的并发数
+    -l, --rateLimit <n>         两个任务之间的最小间隔
+    -d, --dir [value]           书籍存放目录
+```
 3. `npm run epub`
+
+### Test Command
+>node index.js -o ixiaos -n 3185 -s touxiang -c 3 -l 1000 -d ./book/
+
+or
+
+> node index.js -o aishutxt -n 556 -c 3 -l 1000 -d ./book/
--- index.js
+++ index.js
@@ -1,24 +1,30 @@
+const program = require('commander')
 const config = require('./config')
-
-const maxConnections = 3
-const rateLimit = 1000
-const dir = './book/'
-
-// 域名 novel id
-const host = 'http://www.aishutxt.com'
-const novelId = 556
-const staticId = undefined
-
-// 域名 novel id
-// const host = 'http://m.ixiaos.com'
-// const novelId = 3185
-// const staticId = 'touxiang'
-
 const fd = require('./fileDownload')
 
-const novelConfig = config.find(item => item.host === host)
+program
+	// 需要配置
+	.option('-o, --host [value]', 'host') // 可模糊匹配
+	.option('-n, --novelId [value]', '书籍章节链接针对ID动态的')
+	.option('-s, --staticId [value]', '书籍章节链接针对ID静态的')
+	// 一般无需配置
+	.option('-c, --maxConnections <n>', '最大的并发数', parseInt)
+	.option('-l, --rateLimit <n>', '两个任务之间的最小间隔', parseInt)
+	.option('-d, --dir [value]', '书籍存放目录')
+	.parse(process.argv);
+
+const {
+	host,
+	novelId,
+	staticId,
+	maxConnections = 3,
+	rateLimit = 1000,
+	dir = './book/'
+} = program
+
+const novelConfig = config.find(item => item.host.indexOf(host) > -1)
 if (novelConfig) {
-	fd(novelConfig, { maxConnections, rateLimit, dir, host, novelId, staticId }).init()
+	fd(novelConfig, { maxConnections, rateLimit, dir, host: novelConfig.host, novelId, staticId }).init()
 } else {
 	console.error('请先在config.js中配置您的书籍信息~')
 }
